a122ce80214f71b93a9f082852385b16b539dfb5
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2020-07-26 14:30:33 +0900

    引数がNullableであることを追加

--- src/main/java/com/github/mygreen/sqltemplate/type/SqlTemplateValueType.java
+++ src/main/java/com/github/mygreen/sqltemplate/type/SqlTemplateValueType.java
@@ -1,5 +1,7 @@
 package com.github.mygreen.sqltemplate.type;
 
+import org.springframework.lang.Nullable;
+
 /**
  * SQLテンプレート中のパラメータ（変数）の型とJDBCの型を橋渡しするためのインタフェースです。
  *
@@ -17,7 +19,7 @@ public interface SqlTemplateValueType<T> {
      * @return SQLのパラメータ変数。
      * @throws SqlTypeConversionException SQLで扱う型（JDBCの型）の変換にした場合にスローされます。
      */
-    Object getBindVariableValue(T value) throws SqlTypeConversionException;
+    Object getBindVariableValue(@Nullable T value) throws SqlTypeConversionException;
 
     /**
      * SQLに直接埋め込む値として文字列に変換します。
@@ -26,7 +28,7 @@ public interface SqlTemplateValueType<T> {
      * @return 文字列に変換した値
      * @throws TextConversionException 値を文字列への変換に失敗したときにストローされます。
      */
-    default String getEmbeddedValue(final T value) throws TextConversionException {
+    default String getEmbeddedValue(final @Nullable T value) throws TextConversionException {
         if(value == null) {
             return null;
         }

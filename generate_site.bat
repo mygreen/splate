fdac5f6ce9d16a6625c46a5d1dbf406372dd220a
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2021-01-30 13:48:04 +0900

    変数をenv.batから読み込むよう変更

--- generate_site.bat
+++ generate_site.bat
@@ -3,16 +3,7 @@
 %~d0
 cd %~p0
 
-if NOT "%JAVA_HOME_11%" == "" (
-    set JAVA_HOME="%JAVA_HOME_11%"
-)
-
-if NOT "%M2_HOME%" == "" (
-    set M2_HOME="%M2_HOME%"
-)
-
-set PATH=%PATH%;%JAVA_HOME%\bin;%M2_HOME%\bin;
-
+call env.bat
 
 call mvn -version
 
